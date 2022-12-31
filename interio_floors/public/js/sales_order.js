da4b92f8efd5dec03d5731f1f45a7a9608a5481b
Shahzad Naser <shahzadnaser1122@gmail.com>
2022-12-31 15:16:43 +0500

    update cusotmization

---
+++ interio_floors/public/js/sales_order.js
@@ -0,0 +1,45 @@
+frappe.ui.form.on('Sales Order', {
+    update_item_details: function(item){
+        console.log(item);
+        return cur_frm.call({
+            method: "interio_floors.events.events.get_item_details",
+            child: item,
+            args: {
+                "item_code": item.item_code
+            },
+            callback: function(r) {
+                if(r.message){
+                    frappe.model.set_value(item.doctype, item.name,"per_piece", flt(r.message.per_piece));
+                    frappe.model.set_value(item.doctype, item.name,"per_box", flt(r.message.per_box));
+
+                    var qty = flt(flt(item.boxes * r.message.per_box) + flt(item.pieces * r.message.per_piece));
+                    frappe.model.set_value(item.doctype, item.name,"qty",qty);
+                }
+            }
+        });
+
+    }
+});
+frappe.ui.form.on('Sales Order Item', {
+	item_code: function(frm,cdt, cdn) {
+		var item = frappe.get_doc(cdt, cdn);
+        frm.events.update_item_details(item)
+    },
+    pieces: function(frm, cdt, cdn){
+		var item = frappe.get_doc(cdt, cdn);
+        var qty = flt(flt(item.boxes * item.per_box) + flt(item.pieces * item.per_piece));
+        frappe.model.set_value(item.doctype, item.name,"qty",qty);    
+    },
+    boxes: function(frm, cdt, cdn){
+		var item = frappe.get_doc(cdt, cdn);
+        var qty = flt(flt(item.boxes * item.per_box) + flt(item.pieces * item.per_piece));
+        frappe.model.set_value(item.doctype, item.name,"qty",qty);     
+        console.log("=============================")
+    },
+    // qty: function(frm, cdt, cdn){
+    //     var item = frappe.get_doc(cdt, cdn);
+    //     var qty = flt(flt(item.boxes * r.message.per_box) + flt(item.pieces * r.message.per_piece));
+    //     frappe.model.set_value(item.doctype, item.name,"qty",qty);    
+    // },
+    
+    });
