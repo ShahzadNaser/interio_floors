b421418a13e0a056c027e0e095b434825ab971ff
Shahzad Naser <shahzadnaser1122@gmail.com>
2023-01-08 09:26:24 +0500

    update hooks

--- interio_floors/hooks.py
+++ interio_floors/hooks.py
@@ -30,7 +30,13 @@ app_license = "MIT"
 
 # include js in doctype views
 doctype_js = {
-    "Sales Order" : "public/js/sales_order.js"
+    "Quotation" : "public/js/quotation.js",
+    "Sales Order" : "public/js/sales_order.js",
+    "Delivery Note" : "public/js/delivery_note.js",
+    "Sales Invoice" : "public/js/sales_invoice.js",
+    "Purchase Order" : "public/js/purchase_order.js",
+    "Purchase Receipt" : "public/js/purchase_receipt.js",
+    "Purchase Invoice" : "public/js/purchase_invoice.js",
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
