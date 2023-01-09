854dab33c7d6d283d8f6aa6eba2dc71dbaa0955d
Shahzad Naser <shahzadnaser1122@gmail.com>
2023-01-09 22:20:26 +0500

    update events

--- interio_floors/events/events.py
+++ interio_floors/events/events.py
@@ -5,10 +5,10 @@ from erpnext.stock.doctype.batch.batch import get_batch_no
 def get_item_details(item_code=None, warehouse=None):
     if not item_code:
         return False
-    result = frappe.db.get_value("Conversion Details",{"parent":item_code},["per_piece","per_box"],as_dict=True)
+    result = frappe.db.get_value("Conversion Details",{"parent":item_code},["per_piece","per_box","box_size"],as_dict=True)
     if not result:
         item_group = frappe.db.get_value("Item", item_code, "item_group")
-        result = frappe.db.get_value("Conversion Details",{"parent":item_group},["per_piece","per_box"],as_dict=True)
+        result = frappe.db.get_value("Conversion Details",{"parent":item_group},["per_piece","per_box","box_size"],as_dict=True)
 
     return result
 
